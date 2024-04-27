bcbbee88ea678261c29a85f951b3e9105ea88d5d
vaibhav thapli <vaibhavthapli123@gmail.com>
2024-04-27 23:25:39 +0530

    steup

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 from setuptools import setup, find_packages
+
+
+
+
+setup(
+    name = "sensor",
+    version="0.0.1",
+    author="vaibhavthapli",
+    author_email="hellovaibt@gmail.com",
+    packages = find_packages(),
+    install_requires = get_reqirements(),
+)
